829556d8db9450f8c81b6ea2f7fffabdf39bc9f5
Aymen <aymenbouazra994@gmail.com>
2024-04-23 16:33:41 +0100

    stats dashboard

--- backend/controllers/dashboard.controller.js
+++ backend/controllers/dashboard.controller.js
@@ -2,7 +2,6 @@ const User = require('../models/user');
 const Project = require('../models/project');
 const Task = require('../models/task');
 const Team = require('../models/team');
-const Absence = require('../models/absence_sheet');
 
 exports.dashboard = async (req, res) => {
     try {
@@ -10,12 +9,33 @@ exports.dashboard = async (req, res) => {
         const employees = await User.countDocuments({ role: 'employe' });
         const chefs = await User.countDocuments({ role: 'chef' });
         const usersWithTeam = await User.countDocuments({ team: { $exists: true } });
-        const projects = await Project.countDocuments();
         const teams = await Team.countDocuments();
+        const teamsAndTheirProjects = await Team.find()
+        const projects = await Project.countDocuments();
+        const projectsNotStarted = await Project.countDocuments({ status: 'En attente' });
+        const projectsInProgress = await Project.countDocuments({ status: 'En cours' });
         const projectsFinished = await Project.countDocuments({ status: 'Terminé' });
         const tasks = await Task.countDocuments();
+        const tasksNotStrated = await Task.countDocuments({ Status: 'En attente' });
+        const tasksInProgress = await Task.countDocuments({ Status: 'En cours' });
         const tasksFinished = await Task.countDocuments({ Status: 'Terminé' });
-        res.json({ users, usersWithTeam, teams, employees, chefs, projects, projectsFinished, tasks, tasksFinished })
+        res.json({
+            users,
+            usersWithTeam,
+            teams,
+            teamsAndTheirProjects,
+            employees,
+            chefs,
+            projects,
+            projectsNotStarted,
+            projectsInProgress,
+            projectsFinished,
+            tasks,
+            tasksNotStrated,
+            tasksInProgress,
+            tasksFinished,
+
+        })
     } catch (error) {
         res.status(500).json({ message: error.message || "error server" });
     }
