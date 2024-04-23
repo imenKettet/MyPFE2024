5dcfb0586e31f75012ba9fbf89c4347255ac2a89
Aymen <aymenbouazra994@gmail.com>
2024-04-23 16:50:20 +0100

    fixed teamName in list fiche de temps

--- frontend/src/components/pages/ListTimeSheet.jsx
+++ frontend/src/components/pages/ListTimeSheet.jsx
@@ -229,7 +229,7 @@ const ListTimeSheet = () => {
                     {taskIndex === 0 && (
                       <td rowSpan={project.tasks.length} className="border text-dark bg-light-warning fs-2 text-center align-middle">
                         <div className="d-flex flex-column align-items-center">
-                          {project.nameProject} <span className="text-success h6">{localStorage.getItem('role') === 'admin' && (project.teams[projectIndex].teamName)}</span>
+                          {project.nameProject} <span className="text-success h6">{localStorage.getItem('role') === 'admin' && (project.teams[projectIndex]?.teamName ? project.teams[projectIndex].teamName : 'Non affecté')}</span>
                         </div>
                       </td>
                     )}
